665798516b09a92a8e0143558b84a3cf83b8927f
TomasGimenezK <tgimenezkesseru@mail.austral.edu.ar>
2026-03-31 11:08:31 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -2,9 +2,25 @@
 
 def max_of_two(x, y):
     """Given x and y, that are 2 numbers, return the biggest number."""
-    return "ANSWER HERE" # Remove this line and implement
 
+    if x>y:
+        return x
+    elif y>x:
+        return y
+    else:
+        return x
 
 def max_of_three(x, y, z):
     """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return "ANSWER HERE" # Remove this line and implement
+    if x >= y and x >= z:
+        return x
+    elif y >= z and y > x:
+        return y
+    elif z > x and z > y:
+        return z
+
+
+
+
+print(max_of_three(0,0,0))
+print(max_of_two(0,0))
